1577e24622ebede1b8f8d653905235b6dfeefe02
Semir Smajlovic <Semirxbih@gmail.com>
2022-03-07 21:11:03 -0800

    Updates

--- testScanner.h
+++ testScanner.h
@@ -1,10 +1,4 @@
-/*
- * testScanner.h by Pascal Odijk 2/24/2021
- * P1 CS4280 Professor Hauschild
- *
- * This file contains the prototype for the testScanner function in testScanner.cpp.
- */
-
+// Define our includes
 #ifndef TESTSCANNER_H
 #define TESTSCANNER_H
 
